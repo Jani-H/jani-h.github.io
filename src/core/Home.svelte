e419295855efb2e50458e006363acbc4207bccc9
Jani-H <AA4087@student.jamk.fi>
2024-09-25 14:17:07 +0300

    replaced home with the content from about page and removed about page.

--- src/core/Home.svelte
+++ src/core/Home.svelte
@@ -3,42 +3,108 @@
 </script>
 
 <main transition:slide>
-  <h1>Welcome to my portfolio!</h1>
-  <p>
-    You can find info about me and projects I have worked on from the menu
-    above.
-  </p>
+  <div class="info-box">
+    <h2>About me</h2>
+    <p>
+      I am 33 year old student and I currently live at Ainola, Jyväskylä. I
+      study Business Information Technology at Jyväskylä University of Applied
+      Sciences. I hope to specialize in programming and web design.
+    </p>
+  </div>
+  <div class="info-box">
+    <h2>This is me.</h2>
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img id="me" src="./images/me.png" alt="Picture of Me" />
+  </div>
+  <div class="info-box">
+    <h2>Hobbies and Freetime</h2>
+    <p>
+      On my freetime I like playing video games with my friends, grilling at
+      friends place and doing other things. I also enjoy traveling and seeing
+      new places.
+    </p>
+  </div>
+  <div class="info-box">
+    <h2>Skills</h2>
+    <h3>Languages</h3>
+    <p>Finnish - Native Language</p>
+    <p>English - Excellent</p>
+  </div>
+  <div class="info-box">
+    <h2>Career</h2>
+    <div>
+      <h3>Saimaan Mediakeskus, full-time job 2018-2019</h3>
+      <p>digi-assistant</p>
+      <p>
+        My job description included taking care of Lauritsala area’s schools
+        information and communication related hardware.
+      </p>
+      <p>Job duration 8 months</p>
+    </div>
+    <br />
+    <div>
+      <h3>Saimaan talous ja tieto Oy, summer job 2015</h3>
+      <p>IT-support intern</p>
+      <p>
+        My job description included doing local and remote support, also phone
+        support at a service point.
+      </p>
+      <p>Job duration 3 months</p>
+    </div>
+  </div>
+  <div class="info-box">
+    <h2>Education</h2>
+    <div>
+      <h3>Jyväskylä University of Applied Sciences, XX.XX.20XX</h3>
+      <p>Tietojenkäsittely tradenomi tutkinto-ohjelma</p>
+    </div>
+    <br />
+    <div>
+      <h3>Saimaan ammattiopisto Sampo, 31.03.2016</h3>
+      <p>Tieto- ja viestintätekniikan perustutkinto</p>
+    </div>
+  </div>
 </main>
 
 <style>
   main {
-    text-align: center;
-    padding-top: 20vh;
-    margin: 1em 4em;
+    margin: 1em 15em;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 0.25em;
+    row-gap: 0.25em;
+    justify-content: center;
+  }
+
+  .info-box {
+    margin: 0em;
     border-style: solid;
     border-color: black;
     border-width: 1px;
     border-radius: 5px;
     flex: 1;
-    padding-top: 0em;
-    padding-bottom: 3em;
-    padding-left: 2em;
-    padding-right: 2em;
+    padding: 0.5em 0.5em;
+    min-width: 20em;
+    box-sizing: border-box;
     background-color: rgba(0, 0, 0, 0.5);
   }
 
-  h1 {
-    padding: 1em;
-    text-align: center;
+  h2 {
+    text-decoration: underline;
   }
 
   p {
     font-size: 1.2em;
   }
 
-  @media screen and (max-width: 425px) {
+  img {
+    border-radius: 10px;
+  }
+
+  @media screen and (max-width: 700px) {
     main {
       margin: 1em 1em;
+      grid-template-columns: 1fr;
     }
   }
 </style>

9c792849f0c8bdb2a5665bf143695e4c13e2c740
Jani-H <AA4087@student.jamk.fi>
2024-09-25 14:22:37 +0300

    removed mentions of about page from router

--- src/utils/Router.svelte
+++ src/utils/Router.svelte
@@ -2,13 +2,11 @@
   import Router from 'svelte-spa-router';
 
   import Home from '../core/Home.svelte';
-  import About from '../core/About.svelte';
   import Contact from '../core/Contact.svelte';
   import Works from '../core/Works.svelte';
 
   const routes = {
     '/': Home,
-    '/about': About,
     '/contact': Contact,
     '/projects': Works,
   };
